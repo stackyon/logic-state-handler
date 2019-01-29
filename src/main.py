2f9476e7a76b8b3b0a714ad9624ba3a706162d1d
duncanla <duncanla@mcmaster.ca>
2019-01-29 12:23:27 -0500

    main and tests started

--- src/main.py
+++ src/main.py
@@ -6,9 +6,19 @@ Either the user or AI move is performed each loop
 
 """
 
+import model
+import statecontroller
+import ai
+
+
 terminate = False
 
 while not terminate:
+    if not statecontroller.is_error():
+        player_move = model.get_player_move()
+        model.record_move(player_move)
+        ai.get_ai_move()
+    else:
     # check movement status and do recordmove()
     # update model refresh() and confirm consistency
     # update state refreshstate()
--- src/statecontroller.py
+++ src/statecontroller.py
@@ -1,5 +1,6 @@
 state = State.start;
 maxstate = 5
+error = False
 
 class State:
     start, player, ai, gameover, incorrect = range(1, maxstate)
@@ -8,6 +9,10 @@ def refreshstate():
     # direct between states
     # call on model after each move
 
+def is_error():
+    return error
+
 def error(error_code):
     # throw error light or otherwise
+    error = True
     print('error: ' + error_code)
