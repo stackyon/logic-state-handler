77f191077c79fdb144ded39f25ad8da85c3ff01a
LiamDuncan <duncanla@mcmaster.ca>
2019-02-26 19:15:24 -0500

    tried OO, didn't like it

--- src/movementqueue.py
+++ src/movementqueue.py
@@ -1,19 +1,20 @@
-movement_queue = []
+
+queue = []
 
 
 def add_move(move):
     for movement in move.movements:
-        movement_queue.append(movement)
+        queue.append(movement)
 
 
 def add_movement(movement):
-    movement_queue.append(movement)
+    queue.append(movement)
 
 
 def print_queue():
-    for movement in movement_queue:
+    for movement in queue:
         if movement.movement_type == 'up':
             start_string = 'pick up at'
         if movement.movement_type == 'down':
             start_string = 'put down at'
-        print(start_string + ' (' + str(movement.square_x) + ', ' + str(movement.square_y) + ')')
+        return start_string + ' (' + str(movement.square_x) + ', ' + str(movement.square_y) + ')'
